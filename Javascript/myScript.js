eb3d2a6853807b9b5f1c3263c26aadc954ffd142
emlil <46994670+emlil@users.noreply.github.com>
2019-04-09 18:22:52 +0200

    satt opp begynnelsen på requestene

--- Javascript/myScript.js
+++ Javascript/myScript.js
@@ -1,3 +1,52 @@
-function onStart() {
-    
+
+async function getUtdanning() {
+    /*let job =  new Promise(
+      function(resolve, reject) {
+          // Fetch data with json
+        let failed = true;
+        if (failed) {
+            reject("It bork");
+        }
+
+        resolve("data");
+      }
+    );
+
+    console.log("HELLO");
+
+    return job;*/
+    let call = await fetch("http://wildboy.uib.no/~tpe056/folk/104857.json")
+    let data = await call.json();
+    return data;
 }
+
+function getBefolkning() {
+
+}
+
+function getSysselsatte() {
+
+}
+
+async function onStart() {
+
+    try {
+        //let answer = await getUtdanning();
+
+        getUtdanning().then(
+            function(answer) {
+                console.log(answer);
+            }
+        ).catch(function(error) {
+            console.log("something bad happened");
+        });
+        //console.log(answer);
+    } catch(e) {
+        console.log("CAUGHT EXCEPTION", e);
+    }
+    //await getSysselsatte();
+    //await getUtdanning();
+
+}
+
+onStart();
