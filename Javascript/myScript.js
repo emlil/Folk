ce577c2e9e7ad818eaaf8914b522aa0d835ac2ab
Jorgen Solum <j-solum@live.com>
2019-04-14 19:08:51 +0200

    ikke noe

--- Javascript/myScript.js
+++ Javascript/myScript.js
@@ -1,104 +1,67 @@
-
-async function getUtdanning() {
-    /*let job =  new Promise(
-      function(resolve, reject) {
-          // Fetch data with json
-        let failed = true;
-        if (failed) {
-            reject("It bork");
-        }
-
-        resolve("data");
-      }
-    );
-
-    console.log("HELLO");
-
-    return job;*/
-    let call = await fetch("http://wildboy.uib.no/~tpe056/folk/85432.json");
-    let data = await call.json();
-    return data;
+function GenConstruct(datasett){
 }
 
-async function getBefolkning() {
-let call = await fetch("http://wildboy.uib.no/~tpe056/folk/104857.json");
-   call= await call.json();
-return call["elementer"];
-}
-
-async function getSysselsatte() {
-    let call = await fetch("http://wildboy.uib.no/~tpe056/folk/100145.json");
-    call=await call.json();
-    return call["elementer"]
-}
-
-function GenConstruct(){
-
-}
-
-function protoTest(){
-  var res = Object.create(genConstruct)
+GenConstruct.prototype.getJson=function(url){
+  var xhr = new XMLHttpRequest();
+  xhr.open("GET", url)
+  xhr.onreadystatechange = function() {
+    if(xhr.readyState === 4 && xhr.status === 200){
+      var response = JSON.parse(xhr.responseText);
+      console.log(response)
+      return response
+    }
+  }
+  xhr.send();
 }
 
 GenConstruct.prototype.getNames=function(){
-      let arr=[];
-      for (elementer in this.datasett){
-          arr.push(elementer);
-      }
-      return arr;
+  let arr=[];
+  for (elementer in this.datasett){
+      arr.push(elementer);
+    }
+  return arr;
   }
 
 GenConstruct.prototype.getDetalj=function(){
-      let nrInn= document.getElementById("detaljNr").value;
-      console.log(nrInn);
-      document.getElementById("detaljData").innerHTML=befolkObj.getInfo(nrInn);
-    }
+  let nrInn= document.getElementById("detaljNr").value;
+  console.log(nrInn);
+  document.getElementById("detaljData").innerHTML=befolkObj.getInfo(nrInn);
+  }
 
 GenConstruct.prototype.getInfo=function(kommNr){
   for (elementer in this.datasett){
-      if(this.datasett[elementer]["kommunenummer"]===kommuneNr){
-          let out="";
-          out=elementer.toString()+JSON.stringify(this.datasett[elementer]);
-          return out;
-      }
+    if(this.datasett[elementer]["kommunenummer"]===kommuneNr){
+        let out="";
+        out=elementer.toString()+JSON.stringify(this.datasett[elementer]);
+        return out;
     }
+  }
   return "error";
 }
 
 
- function BefolkningConstruct(datasett) {
-    this.url="http://wildboy.uib.no/~tpe056/folk/104857.json";
-    this.datasett= datasett;
-    
+ function befolkningConstruct(datasett) {
+    var x = Object.create(GenConstruct.prototype)
+    var tabell = x.getJson(datasett)
+    console.log(tabell)
  }
 
-async function onStart() {
-    let sysselsatt;
-    let utdanning;
-    let befolkning;
-    try {
-         utdanning = await getUtdanning();
-        console.log(utdanning);
-    } catch(e) {
-        console.log("CAUGHT EXCEPTION", e);
-    }
-    try {
-         sysselsatt = await getSysselsatte();
-        console.log(sysselsatt);
-    }
+ function sysselsattConstruct(datasett) {
+    var y = Object.create(GenConstruct.prototype)
+    y.getJson(datasett)
 
-    catch(e) {
-        console.log("CAUGHT EXCEPTION", e);
-    }
+ }
 
-     befolkning= await getBefolkning();
-     console.log(befolkning);
-     befolkObj= new BefolkningConstruct(befolkning);
-    //sysselObj = new sysselsattConstruct(syssel);
-    //console.log(befolkObj.getNames())
-    //console.log(befolkObj.getInfo("0101"))
+ function utdanningConstuct(datasett) {
+    var z = Object.create(GenConstruct.prototype)
+    z.getJson(datasett)
 
+ }
 
+function onStart(){
+     befolkObj= new befolkningConstruct("http://wildboy.uib.no/~tpe056/folk/104857.json");
+    sysselObj = new sysselsattConstruct("http://wildboy.uib.no/~tpe056/folk/100145.json");
+    utdanningConstuct = new utdanningConstuct("http://wildboy.uib.no/~tpe056/folk/85432.json")
 }
 let befolkObj;
 
