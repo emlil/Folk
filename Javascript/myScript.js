d7c663296da737dd13b748fbc8775ad95787efc7
emil <emildanielsen97@gmail.com>
2019-04-20 12:23:46 +0200

    gjort datahenting funksjonene mer dynamisk
    sender nå ønsket år som argument.

--- Javascript/myScript.js
+++ Javascript/myScript.js
@@ -62,13 +62,13 @@ async function getSysselsatte() {
 //getInfo mottar et input i fra brukeren, thrower error hvis kommunenummeret
 //ikke eksisterer i objectet. Den returnerer en string med elementet hvis den
 //finner det.
-    this.getInfo=function (kommuneNr) {
+    this.getInfo=function (kommuneNr,aar) {
         for (elementer in this.datasett){
             if(this.datasett[elementer]["kommunenummer"]===kommuneNr){
              //Oppretter et objekt for valgte kommune slik at vi kan ta ut infoen etter objektet er returnert.
                 //Slik kan vi presentere dataen slik vi ønsker
                 let thisKommune ={}
-                thisKommune.befolkning=this.datasett[elementer]["Kvinner"]["2018"]+this.datasett[elementer]["Menn"]["2018"]
+                thisKommune.befolkning=this.datasett[elementer]["Kvinner"][aar]+this.datasett[elementer]["Menn"][aar]
                 thisKommune.navn=elementer;
                 thisKommune.nummer=kommuneNr;
                 console.log(thisKommune)
@@ -93,9 +93,9 @@ function SysselsattConstruct(datasett) {
       return arr;
   };
 
-  this.sysselSattePros = function(kommuneNavn){
+  this.sysselSattePros = function(kommuneNavn,aar){
     //bruker kommunenavn for å hente ut data.
-    var prosent=(this.datasett[kommuneNavn]["Begge kjønn"]["2018"]).toFixed(2);
+    var prosent=(this.datasett[kommuneNavn]["Begge kjønn"][aar]).toFixed(2);
     return prosent;
   }
 
@@ -112,39 +112,52 @@ function UtdaninngConstuct(datasett) {
     return arr
   }
 
-
-  this.getHoyUtdanning=function(kommune){
+  this.getHoyUtdanning=function(kommune,aar){
     //antar at høyere utdanning kun er 03a og 04a
     //SE ØVE OM DENNE MATTEN E RIKTIG
-    kommune.utdanningProsent =(this.datasett["elementer"][kommune.navn]['03a']["Kvinner"]['2017']+this.datasett["elementer"][kommune.navn]['03a']["Menn"]['2017'])/2;
-    kommune.utdanningProsent+=((this.datasett["elementer"][kommune.navn]['04a']["Kvinner"]['2017']+this.datasett["elementer"][kommune.navn]['04a']["Menn"]['2017'])/2);
+    kommune.utdanningProsent =(this.datasett["elementer"][kommune.navn]['03a']["Kvinner"][aar]+this.datasett["elementer"][kommune.navn]['03a']["Menn"][aar])/2;
+    kommune.utdanningProsent+=((this.datasett["elementer"][kommune.navn]['04a']["Kvinner"][aar]+this.datasett["elementer"][kommune.navn]['04a']["Menn"][aar])/2);
     kommune.utdanningProsent=kommune.utdanningProsent.toFixed(2)
     kommune.utdanningAntall=kommune.befolkning*(kommune.utdanningProsent)/100
     return kommune;
   }
 }
-//funksjon for å fremvise data
-function fremvis(loc,object){
 
-    document.getElementById(loc).innerHTML=object;
-}
 //getDetalj Henter data fra datasettene og legger dem til det opprettede objektet kommune slik at de er tilgjengelige for flere handlinger
 function getDetalj(){
      let nrInn= document.getElementById("detaljNr").value;
   
     //Oppretter obbjekt for kommunen det skal hentes data fra 
-    var kommune=befolkObj.getInfo(nrInn);
-   fremvis("detaljData",kommune['navn']+" ("+kommune['nummer']+")</br> Sist målte befolkning: "+kommune['befolkning']);
+    let kommune=befolkObj.getInfo(nrInn,2018);
+    document.getElementById("detaljData").innerHTML=kommune['navn']+" ("+kommune['nummer']+")</br> Sist målte befolkning: "+kommune['befolkning'];
     
-    kommune.prosent=sysselObj.sysselSattePros(kommune['navn'])
+    kommune.prosent=sysselObj.sysselSattePros(kommune['navn'],2018)
     document.getElementById("pross").innerHTML="Prosent sysselsetting: "+kommune['prosent']+"%";
     
     //bruker Math.floor her for å få hele tall uten desimal fordi kun hele mennesker jobber
     kommune.antallSysselsatt=Math.floor((kommune['prosent']/100)*kommune['befolkning']);
     document.getElementById("totalSyssel").innerHTML ="Totalt antall sysselsatt: "+kommune['antallSysselsatt'];
 
-    kommune=utdanningObj.getHoyUtdanning(kommune);
+    kommune=utdanningObj.getHoyUtdanning(kommune,2017);
     document.getElementById("utdanning").innerHTML="Prosent utdanning "+kommune.utdanningProsent+"</br> Antall utdannet: "+kommune.utdanningAntall;
+
+    //historiskUtviklingTabell(kommune['navn'])
+ }
+
+ //funksjon som settter sammen data, og bygger tabell
+ function historiskUtviklingTabell(kommune){
+    let tabell= "<table><tr><th>År</th><th>Befolkning</th><th>Sysselsetting</th><th>Utdanning</th></tr>";
+    //oppretter en tabell. plasserer 4 arrays inni dette. en for hvert sett med verdier. Fordi de kun finnes verdier i tidsrommet 2007-2017 er det disse verdiene vi vil fremvise
+    let arr=[];
+    let aarArray=[];
+    for (let index = 2007; index <= 2017; index++) {
+      aarArray.push(index);
+    }
+    arr.push(aarArray);
+    
+    
+
+    tabell+="</table>"
  }
 
 //getOversikt henter ut befolkning for alle kommunene, legger sammen
@@ -177,19 +190,15 @@ async function onStart() {
          sysselsatt = await getSysselsatte();
         console.log(sysselsatt);
     }
-
     catch(e) {
         console.log("CAUGHT EXCEPTION", e);
     }
-
      befolkning = await getBefolkning();
      syssel = await getSysselsatte();
      console.log(befolkning);
      befolkObj = new BefolkningConstruct(befolkning);
      sysselObj = new SysselsattConstruct(syssel)
      utdanningObj = new UtdaninngConstuct(utdanning)
-    //console.log(befolkObj.getNames())
-    //console.log(befolkObj.getInfo("0101"))
 }
 
 let befolkObj;
